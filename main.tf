4f64b69a5f355f9cd195f44057fd4bec7016aae6
dadda5 <daddaop5@gmail.com>
2026-07-04 00:47:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "Flipkart" {
+resource "aws_instance" "flipkart" {
  ami = "ami-01a00762f46d584a1"
 }
 
- resource "aws_s3_bucket" "Storage" {
+ resource "aws_s3_bucket" "storage" {
   bucket = var.bucket_name"
   tags = {
   Name = var.project_name
