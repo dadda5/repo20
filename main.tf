6c9efc9c2c1a637a94dac787c5a983ceaf71bd97
dadda5 <daddaop5@gmail.com>
2026-07-04 00:35:56 +0530

    Fix resource declaration syntax in main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resources "AWS_instance" "Flipkart"
- ami = ami-01a00762f46d584a1
+resource "aws_instance" "Flipkart" {
+ ami = "ami-01a00762f46d584a1"
+}
 
- resources "AWS_s3_bucket" "Storage" {
-  bucket = var.bucket_name
-  tag = {
+ resource "aws_s3_bucket" "Storage" {
+  bucket = var.bucket_name"
+  tags = {
   Name = var.project_name
     }
   }
